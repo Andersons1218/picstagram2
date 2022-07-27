17fa31bcf33931372e8c9b6833b21fd8c57ec875
Anderson Santana <104103694+Andersons1218@users.noreply.github.com>
2022-07-27 14:18:04 -0400

    created post model

---
+++ models/post.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const postSchema = mongoose.Schema(
+	{
+		user: { 
+			type: Schema.Types.ObjectId, 
+			ref: 'User',
+			required: true,
+		},
+		likes: {
+			type: [],
+			require: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		image: String,
+		updatedAt: {
+			type: Date,
+			default: new Date(),
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model("Post", postSchema)
